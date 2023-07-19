862893a93d02acb1c10009e10cb33a841ca8795f
Ashutosh Anand <ashu2002anand>
2023-07-20 01:51:44 +0530

    Doubly_LL creation

--- Doubly_LL.c
+++ Doubly_LL.c
@@ -18,7 +18,7 @@ void Create(int A[],int n)
     first->prev=first->next=NULL;
     last=first;
 
-    for (int i = 0; i < n-1; i++)
+    for (int i = 1; i < n; i++)
     {
         t=(struct Node *)malloc(sizeof(struct Node));
         t->data=A[i];
@@ -49,10 +49,87 @@ int length(struct Node *p){
     return len;
 }
 
+void Insert(struct Node *p,int index,int x){
+    struct Node *t;
+    t=(struct Node *)malloc(sizeof(struct Node));
+    t->data=x;
+
+    if(index < 0 || index > length(p))
+        return;
+
+    if (index==0){
+        t->prev=NULL;
+        t->next=first;
+        first->prev=t;
+        first=t;
+    }
+    else
+    {
+        for (int i = 0; i < index-1; i++)
+            p=p->next;
+
+        t->prev=p;
+        t->next=p->next;
+        if (p->next)p->next->prev=t;
+        p->next=t;
+    }
+}
+
+int delete(struct Node *p,int index){
+    struct Node *q;
+    int x=-1;
+    if(index <1 || index > length(p))
+        return x;
+    
+    if(index==1){
+        p=first;
+        first=first->next;
+        if(first)first->prev=NULL;
+
+        x=p->data;;
+        free(p);
+    }
+    else
+    {
+        for (int i = 0; i < index-1; i++)
+            p=p->next;
+        
+        p->prev->next=p->next;
+        if(p->next)p->next->prev=p->prev;
+        x=p->data;
+        free(p);  
+    }
+    
+}
+
+void Reverse(struct Node *p)
+{
+    p=first;
+    struct Node *t;
+    t=(struct Node *)malloc(sizeof(struct Node));
+    while (p)
+    {
+        t=p->next;
+        p->next=p->prev;
+        p->prev=t;
+
+        p=p->prev;
+        if(p!=NULL && p->next==NULL)first=p;
+    }
+    
+}
+
 int main()
 {
-    int A[]={12,23,434,45,655,56,5};
-    Create(A,7);
+    int A[]={12,23,45,67};
+    Create(A,4);
+    Insert(first,2,46);
+    display(first);
+    printf("Length of LL is %d\n",length(first));
+    delete(first,5);
+    display(first);
+    printf("Length of LL is %d\n",length(first));
+    Reverse(first);
     display(first);
     printf("Length of LL is %d\n",length(first));
     return 0;
